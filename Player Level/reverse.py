b65b9234a8f8baf5582818335dc6683ab63035cf
15ec11 <gajapathy027@gmail.com>
2018-02-23 14:49:44 +0530

    Update reverse.py

--- Player Level/reverse.py
+++ Player Level/reverse.py
@@ -1,2 +1,7 @@
-a=int(input("enter the number"))
-print("\n",''.join(list(reversed(str(a)))))
+num=int(input("Enter the number to be reversed:"))
+r=0
+while(num>0):
+    digit=num%10
+    r=r*10+digit
+    num=num//10
+print("Reverse of the given number:",r)
