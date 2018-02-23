0a2edf0b65041b15af8e4e6691752701b1d6b7cf
15ec11 <gajapathy027@gmail.com>
2018-02-23 14:51:14 +0530

    Update reverse.py

--- Player Level/reverse.py
+++ Player Level/reverse.py
@@ -1,7 +1,7 @@
 num=int(input("Enter the number to be reversed:"))
 r=0
 while(num>0):
-    digit=num%10
-    r=r*10+digit
-    num=num//10
+    digit=num%5
+    r=r*5+digit
+    num=num//5
 print("Reverse of the given number:",r)
