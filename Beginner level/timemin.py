bfed12332203d3edb070d1ebe2464b6be532580c
15ec11 <gajapathy027@gmail.com>
2018-07-24 14:37:33 +0530

    Create timemin.py

---
+++ Beginner level/timemin.py
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c,d,e,f;
+    scanf("%d%d",&a,&c);
+    scanf("%d%d",&b,&d);
+    e=a-b;
+    f=c-d;
+    printf("%d\t%d",e,f);
+    
+}
