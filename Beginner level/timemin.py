4b8930a3856294785b7f0029e08e3985acf099b5
15ec11 <gajapathy027@gmail.com>
2018-07-24 14:43:41 +0530

    Update timemin.py

--- Beginner level/timemin.py
+++ Beginner level/timemin.py
@@ -1,12 +1,11 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
+#include <stdio.h>
+int main(void) 
 {
-    int a,b,c,d,e,f;
-    scanf("%d%d",&a,&c);
-    scanf("%d%d",&b,&d);
-    e=a-b;
-    f=c-d;
-    printf("%d\t%d",e,f);
-    
+int hours1,hours2,mins1,mins2,hours3,mins3;
+scanf("%d %d\n",&hours1,&mins1);
+scanf("%d %d\n",&hours2,&mins2);
+hours3=abs(hours1-hours2);
+mins3=abs(mins1-mins2);
+printf("%d %d",hours3,mins3);
+	return 0;
 }
